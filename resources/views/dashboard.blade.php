ad25fd255edd0d074bb0490eeba4ca03efb7179d
“dhruv240901” <dhruvp@zignuts.com>
2023-12-08 19:01:26 +0530

    modify dashboard page

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -8,9 +8,32 @@ $configData = Helper::appClasses();
 
 @section('vendor-script')
 @include('components.toastr')
+<script src="https://cdnjs.cloudflare.com/ajax/libs/lottie-web/5.12.2/lottie.min.js"></script>
 @endsection
 
+
+
 @section('content')
-<h4>Home Page</h4>
-<p>For more layout options refer <a href="{{ config('variables.documentation') ? config('variables.documentation').'/laravel-introduction.html' : '#' }}" target="_blank" rel="noopener noreferrer">documentation</a>.</p>
+    <div class="row">
+        <div class="col-md-10 stretch-card">
+            <div id="welcome-animation">
+
+            </div>
+        </div>
+    </div> <!-- row -->
+
+@endsection
+@section('page-script')
+<script>
+  const animationContainer = document.getElementById('welcome-animation');
+  const animationPath = 'https://lottie.host/8c62dd60-a598-4031-b303-8a8060182b0a/gXCDH6FQYZ.json';
+
+  const animation = lottie.loadAnimation({
+      container: animationContainer,
+      renderer: 'svg',
+      loop: true,
+      autoplay: true,
+      path: animationPath
+  });
+</script>
 @endsection
